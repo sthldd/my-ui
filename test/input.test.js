6220a2a14cef78cbf52ca4212c97f480eecb623c
mlx <1209624528@qq.com>
2018-12-20 22:29:02 +0800

    input props test

---
+++ test/input.test.js
@@ -0,0 +1,58 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+  it('存在', () => {
+    expect(Input).to.be.ok
+  })
+  it('可以接收value参数', () => {
+    const Constructor = Vue.extend(Input)
+    const vm = new Constructor({
+      propsData: {
+        value: '1234'
+      }
+    }).$mount()
+    const inputElement = vm.$el.querySelector('input')
+    expect(inputElement.value).to.equal('1234')
+    vm.$destroy()
+  })
+  it('可以接收disabled参数', () => {
+    const Constructor = Vue.extend(Input)
+    const vm = new Constructor({
+      propsData: {
+        disabled: true
+      }
+    }).$mount()
+    const inputElement = vm.$el.querySelector('input')
+    expect(inputElement.disabled).to.equal(true)
+    vm.$destroy()
+  })
+  it('可以接收readonly参数', () => {
+    const Constructor = Vue.extend(Input)
+    const vm = new Constructor({
+      propsData: {
+        readonly: true
+      }
+    }).$mount()
+    const inputElement = vm.$el.querySelector('input')
+    expect(inputElement.readOnly).to.equal(true)
+    vm.$destroy()
+  })
+  it('可以接收error参数', () => {
+    const Constructor = Vue.extend(Input)
+    const vm = new Constructor({
+      propsData: {
+        error: '你错了'
+      }
+    }).$mount()
+   const useElement = vm.$el.querySelector('use');
+   expect(useElement.getAttribute('xlink:href')).to.equal('#i-error');
+   const errorMessage = vm.$el.querySelector('.errorMessage');
+   expect(errorMessage.innerText).to.equal('你错了');
+   vm.$destroy()
+  })
+})
