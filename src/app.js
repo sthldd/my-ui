c3ba92c4146654d596fead4b26b846d2168a90ee
mlx <1209624528@qq.com>
2018-12-20 21:17:38 +0800

    input组件

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import Vue from "vue";
 import Button from "./button";
 import Icon from "./icon";
 import ButtonGroup from "./button-group";
-
+import Input from "./input";
 
 Vue.component('m-button',Button)
 Vue.component('m-icon', Icon)
 Vue.component('m-button-group', ButtonGroup)
+Vue.component('m-input', Input)
 new Vue({
   el: '#app',
   data:{
