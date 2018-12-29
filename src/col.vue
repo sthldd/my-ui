b429006c26191c4e70fe5be7c398d08a8e7a9026
mlx <1209624528@qq.com>
2018-12-29 14:40:35 +0800

    row and col

---
+++ src/col.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:'MCol',
+  props:{
+    span:{
+      type:[String,Number]
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.col{
+  width: 50%;
+  border:1px solid red;
+  height: 100px;
+  background: gray;
+  $class-prefix:col-;
+  @for $n  from 1 through 24{
+    &.#{$class-prefix}#{$n}{
+      width: ($n / 24 ) * 100%;
+    }
+  }
+}
+</style>
+
+
