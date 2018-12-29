97ced38db23de1499c8d3abddce293adc4462dd4
mlx <1209624528@qq.com>
2018-12-29 17:08:12 +0800

    可以使用

--- src/col.vue
+++ src/col.vue
@@ -1,28 +1,48 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]"
+  :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+  >
+  <div style="border:1px solid ;height:100px;">
     <slot></slot>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name:'MCol',
+  data(){
+    return{
+      gutter:0,
+    }
+  },
   props:{
     span:{
       type:[String,Number]
+    },
+    offset:{
+      type:[String,Number]
     }
   },
+  mounted(){
+    console.log(this.gutter)
+  }
 }
 </script>
 <style lang="scss" scoped>
 .col{
   width: 50%;
-  border:1px solid red;
   height: 100px;
-  background: gray;
+  padding: 0 10px;
   $class-prefix:col-;
   @for $n  from 1 through 24{
     &.#{$class-prefix}#{$n}{
       width: ($n / 24 ) * 100%;
+    }
+  }
+  $class-prefix:offset-;
+  @for $n  from 1 through 24{
+    &.#{$class-prefix}#{$n}{
+      margin-left: ($n / 24 ) * 100%;
     }
   }
 }
