bcb135437d72b889709717d58512c0c53b63df04
mlx <1209624528@qq.com>
2019-01-02 14:10:44 +0800

    支持top bottom middle

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast" ref="toast">
+  <div class="toast" ref="toast" :class="toastClasses">
     <div class="message">
       <slot v-if="!enableHtml"></slot>
       <div v-else v-html="$slots.default[0]"></div> <!-- 去掉slot 用div 因为slot不接受v-html -->
@@ -38,6 +38,20 @@ export default {
     enableHtml:{
       type:Boolean,
       default:false
+    },
+    position:{
+      type:String,
+      default:'top',
+      validator(value){
+        return ['top','bottom','middle'].includes(value)
+      }
+    }
+  },
+  computed:{
+    toastClasses(){
+     return {
+      [`position-${this.position}`] : true
+     }
     }
   },
   mounted(){
@@ -78,9 +92,7 @@ export default {
   $background:rgba(0, 0, 0,0.75);
   .toast{
     position: fixed;
-    top:0;
     left:50%;
-    transform: translateX(-50%);
     font-size: $font-size;
     line-height: $line-height;
     min-height:$height;
@@ -102,6 +114,18 @@ export default {
     }
     .message{
       padding: 8px 0;
+    }
+    &.position-top{
+      top:0;
+      transform: translateX(-50%);
+    }
+    &.position-bottom{
+      bottom:0;
+      transform: translateX(-50%);
+    }
+    &.position-middle{
+      top:50%;
+      transform: translate(-50%,-50%);
     }
   }
 
