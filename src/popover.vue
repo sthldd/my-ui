a9be6e77903406555bb67d32a9e477512f861427
mlx <1209624528@qq.com>
2019-01-06 17:04:17 +0800

    scroolY和scroolX的问题

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click="toggleContent">
-    <div class="content-wrapper"  v-if="visible">
+    <div class="content-wrapper"  v-if="visible" ref="contentWrapper">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 <script>
@@ -17,6 +19,20 @@ export default {
   methods:{
     toggleContent(){
       this.visible = !this.visible
+      if(this.visible == true){
+        this.$nextTick(()=>{
+          document.body.appendChild(this.$refs.contentWrapper)
+          console.log(this.$refs.triggerWrapper)
+          let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+          this.$refs.contentWrapper.style.top = top + window.scrollY +  'px'
+          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+          let clickHandler = ()=>{
+            this.visible = false
+            document.removeEventListener('click',clickHandler)
+          }
+          document.addEventListener('click',clickHandler)
+        })
+      }
     }
   },
 }
@@ -26,13 +42,12 @@ export default {
     display: inline-block;
     vertical-align: top;
     position: relative;
-    >.content-wrapper{
-      position: absolute;
-      bottom: 100%;
-      border:1px solid red;
-      left: 0;
-      box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    }
+  }
+  .content-wrapper{
+    position: absolute;
+    border:1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%);
   }
 </style>
 
