5e8b5db07ae5952497bbdd1e018a548648b4bbc4
mlx <1209624528@qq.com>
2019-02-18 16:27:17 +0800

    完成scroll

--- src/scroll.vue
+++ src/scroll.vue
@@ -1,14 +1,16 @@
 <template>
-    <div  ref="parent" class="m-scroll-wrapper" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
-      <div  ref="child" class="m-scroll">
-        <slot></slot>
-      </div>
-      <div class="m-scroll-track" v-show="scrollBarVisible">
-        <div class="m-scroll-bar" ref="bar" >
-          <div class="m-scroll-bar-inner" draggable="true" @dragstart="onDragStart" @dragend="onDragEnd"></div>
-        </div>
+  <div  ref="parent" class="m-scroll-wrapper" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave" @wheel="onWheel">
+    <div  ref="child" class="m-scroll" :style="{transform:`translateY(${this.contentY}px)`}">
+      <slot></slot>
+    </div>
+    <div class="m-scroll-track" v-show="scrollBarVisible">
+      <div class="m-scroll-bar" ref="bar" @mousedown="onMouseDownScrollBar" @mousemove="onMouseMoveScrollBar"
+           @selectstart="onSelectScrollBar"
+      >
+        <div class="m-scroll-bar-inner"></div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -16,79 +18,116 @@
         name: "MScroll",
         data(){
             return{
-              scrollBarVisible:false
+                scrollBarVisible:false,
+                isScrolling:false,
+                startPosition:undefined,
+                endPosition:undefined,
+                scrollBarY:0,
+                contentY:0,
+                barHeight:undefined,
+                parentHeight:undefined,
+                childHeight:undefined,
             }
         },
         mounted() {
-            let parent = this.$refs.parent
-            let child = this.$refs.child
-            let translateY = 0
-            let {height:parentHeight} = parent.getBoundingClientRect()
-            let {height:childHeight} = child.getBoundingClientRect()
-            let {borderTopWidth,borderBottomWidth,paddingTop,paddingBottom} = window.getComputedStyle(parent)
-            borderTopWidth = parseInt(borderTopWidth)
-            borderBottomWidth = parseInt(borderBottomWidth)
-            paddingTop = parseInt(paddingTop)
-            paddingBottom = parseInt(paddingBottom)
-            let maxHeight = childHeight - parentHeight + (borderTopWidth+borderBottomWidth+paddingTop+paddingBottom)
-            var ua = navigator.userAgent.toLowerCase();
-            parent.addEventListener('wheel',(e)=>{
+            this.listenToDocument()
+            this.parentHeight = this.$refs.parent.getBoundingClientRect().height
+            this.childHeight = this.$refs.child.getBoundingClientRect().height
+            this.updateScrollBar()
+        },
+        methods:{
+            listenToDocument(){
+                document.addEventListener('mouseup',e=>this.onMouseUpScrollBar(e))
+                document.addEventListener('mousemove',e=>this.onMouseMoveScrollBar(e))
+            },
+            onWheel(e){
+                let ua = navigator.userAgent.toLowerCase();
+                let maxHeight = this.calcContentYMax()
                 if(ua.indexOf('Intel Mac OS X')>0){
                     if(e.deltaY > 0){
-                        translateY -= 20 * 3
+                        this.contentY -= 20 * 3
                     }else if(e.deltaY < 0){
-                        translateY -= 20 * 3
+                        this.contentY -= 20 * 3
                     }else{
-                        translateY -= e.deltaY * 3
+                        this.contentY -= e.deltaY * 3
                     }
                 }else{
                     if(e.deltaY > 0){
-                        translateY -= 20 * 3
+                        this.contentY -= 20 * 3
                     }else if(e.deltaY < 0){
-                        translateY += 20 * 3
+                        this.contentY += 20 * 3
                     }else{
-                        translateY -= e.deltaY * 3
+                        this.contentY -= e.deltaY * 3
                     }
                 }
-                if(translateY>0){ //不能继续往上
-                    translateY = 0
-                }else if(translateY  < -maxHeight){ //不能继续往下
-                    translateY = -maxHeight
+                if(this.contentY>0){ //不能继续往上
+                    this.contentY = 0
+                }else if(this.contentY  < -maxHeight){ //不能继续往下
+                    this.contentY = -maxHeight
                 }else{
                     e.preventDefault()
                 }
-                child.style.transform = `translateY(${translateY}px)`
-                this.updateScrollBar(parentHeight,childHeight,translateY)
-            })
-            this.updateScrollBar(parentHeight,childHeight,translateY)
-        },
-        methods:{
-            updateScrollBar(parentHeight,childHeight,translateY){
-               let barHeight = parentHeight * parentHeight / childHeight
-               let bar = this.$refs.bar
-               bar.style.height = barHeight + 'px'
-               let y =parentHeight * translateY / childHeight
-               bar.style.transform = `translateY(${-y}px)`
+                this.updateScrollBar(this.parentHeight,this.childHeight,this.contentY)
+            },
+            calcContentYMax(){
+                let {borderTopWidth,borderBottomWidth,paddingTop,paddingBottom} = window.getComputedStyle(this.$refs.parent)
+                borderTopWidth = parseInt(borderTopWidth)
+                borderBottomWidth = parseInt(borderBottomWidth)
+                paddingTop = parseInt(paddingTop)
+                paddingBottom = parseInt(paddingBottom)
+
+                let maxHeight = this.childHeight - this.parentHeight + (borderTopWidth+borderBottomWidth+paddingTop+paddingBottom)
+                return maxHeight
+            },
+            updateScrollBar(){
+                let parentHeight = this.parentHeight
+                let childHeight = this.childHeight
+                this.barHeight = parentHeight * parentHeight / childHeight
+                this.$refs.bar.style.height = this.barHeight + 'px'
+                let y =parentHeight * this.contentY / childHeight
+                this.$refs.bar.style.transform = `translateY(${-y}px)`
+                this.scrollBarY = -y
             },
             onMouseEnter(){
-               this.scrollBarVisible = true
+                this.scrollBarVisible = true
             },
             onMouseLeave(){
-               this.scrollBarVisible = false
+                this.scrollBarVisible = false
             },
-            onDragStart(){
-              console.log('开始')
+            onMouseDownScrollBar(e){
+                this.isScrolling = true
+                let {screenX,screenY} = e
+                this.startPosition = {x:screenX,y:screenY}
             },
-            onDragEnd(){
-                  console.log('结束')
-            }
-        }
+            onMouseMoveScrollBar(e){
+                if(!this.isScrolling){return}
+                let {screenX,screenY} = e
+                let maxScrollHeight = this.parentHeight - this.barHeight
+                this.endPosition = {x:screenX,y:screenY}
+                let delta = {x:this.endPosition.x - this.startPosition.x,y:this.endPosition.y - this.startPosition.y}
+                this.scrollBarY = parseInt(this.scrollBarY) + delta.y
+                if(this.scrollBarY < 0){
+                    this.scrollBarY = 0
+                }else if(this.scrollBarY > maxScrollHeight){
+                    this.scrollBarY = maxScrollHeight
+                }
+                this.contentY = -(this.childHeight * this.scrollBarY / this.parentHeight)
+                this.startPosition = this.endPosition
+                this.$refs.bar.style.transform = `translate(0px,${this.scrollBarY}px)`
+            },
+            onMouseUpScrollBar(e){
+                this.isScrolling = false
+            },
+            onSelectScrollBar(e){
+                e.preventDefault()
+            },
+        },
     }
 </script>
 
 <style scoped lang="scss">
   .m-scroll{
-    transition: transform 0.05s ease;
+    transition: transform 0.01s ease;
     &-wrapper{
       margin:0 auto;
       border:1px solid green;
